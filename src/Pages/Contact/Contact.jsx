23b2c775e03ccc90b4c692471f8d9c1c24eeac51
imtiaz hussain <imtiazh993@gmail.com>
2023-03-24 22:13:30 -0700

    contact & portfolio details page added

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,9 +1,129 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div className="bg-black">
+      <div className="relative">
+        <img src="/images/Contact/Hero.png" alt="" />
+        <h1 className="absolute top-1/3 left-28 text-7xl text-white">
+          Contact Us
+        </h1>
+      </div>
+      <div className="w-3/4 mx-auto">
+        <div className="flex justify-between mt-20 border-b border-white pb-4 mb-16">
+          <div className="w-1/4">
+            <div className="mb-8">
+              <h1 className="text-white text-xl mb-3">Canada</h1>
+              <div className="flex items-start">
+                <img className="mr-2" src="/icons/location.svg" alt="" />
+                <p className="text-white">
+                  810 Quayside Dr #205, New Westminster, BC V3M 6B9
+                </p>
+              </div>
+            </div>
+            <div className="mb-8">
+              <h1 className="text-white text-2xl mb-3">India</h1>
+              <div className="flex items-start">
+                <img className="mr-2" src="/icons/location.svg" alt="" />
+                <p className="text-white">
+                  National Car Bazar, Johal Market, Zira, Punjab 142047
+                </p>
+              </div>
+            </div>
+            <div className="mb-8">
+              <h1 className="text-white text-2xl mb-3">Qatar</h1>
+              <div className="flex items-start">
+                <img className="mr-2" src="/icons/location.svg" alt="" />
+                <p className="text-white">
+                  Al Mana Business Centre 02, Doha, Qatar
+                </p>
+              </div>
+            </div>
+            <div className="mb-8">
+              <h1 className="text-white text-2xl mb-3">Social Media</h1>
+              <div className="flex">
+                <div className="rounded-full border border-white mr-3 w-12 h-12 flex items-center justify-center">
+                  <img className="w-1/2" src="/icons/twitter.svg" alt="" />
+                </div>
+                <div className="rounded-full border border-white mr-3 w-12 h-12 flex items-center justify-center">
+                  <img className="w-1/2" src="/icons/insta.svg" alt="" />
+                </div>
+                <div className="rounded-full border border-white mr-3 w-12 h-12 flex items-center justify-center">
+                  <img className="w-1/2" src="/icons/linkedIn.svg" alt="" />
+                </div>
+                <div className="rounded-full border border-white mr-3 w-12 h-12 flex items-center justify-center">
+                  <img className="w-1/2" src="/icons/whatsapp.svg" alt="" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="w-4/6">
+            <h1 className="text-white text-4xl mb-8">Send Us a Message</h1>
+            <div className="flex justify-between mb-5">
+              <input
+                className="w-1/2 mr-8 text-white bg-transparent border border-white rounded-lg p-3"
+                type="text"
+                placeholder="Name *"
+                required
+              />
+              <input
+                className="w-1/2 text-white bg-transparent border border-white rounded-lg p-3"
+                type="tel"
+                placeholder="Phone"
+              />
+            </div>
+            <div className="flex justify-between mb-5">
+              <input
+                className="w-1/2 mr-8 text-white bg-transparent border border-white rounded-lg p-3"
+                type="email"
+                placeholder="Email *"
+                required
+              />
+              <input
+                className="w-1/2 text-white bg-transparent border border-white rounded-lg p-3"
+                type="text"
+                placeholder="Subject"
+              />
+            </div>
+            <div className="mb-5">
+              <textarea
+                rows={6}
+                className="w-full resize-none text-white bg-transparent border border-white rounded-lg p-3"
+                type="email"
+                placeholder="Message *"
+                required
+              />
+            </div>
+            <button className="text-white border-2 border-white rounded-3xl py-2 px-5">
+              Send Message
+            </button>
+          </div>
+        </div>
+        <div>
+          <h1 className="text-white text-4xl mb-10">Office Locations</h1>
+          <div className="mt-8">
+            <p className="text-white text-xl mb-8">Canada</p>
+            <div class="mapouter">
+              <div class="gmap_canvas">
+                <iframe width="100%" height="100%" id="gmap_canvas" src="https://maps.google.com/maps?q=Westminste &t=&z=10&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+                <a href="https://2yu.co">2yu</a>
+                 <br/>
+                {/* < style>.mapouter{position:relative;text-align:right;height:100%;width:100%;}</style> */}
+                <a href="https://embedgooglemap.2yu.co/">html embed google map</a>
+                {/* <style>.gmap_canvas {overflow:hidden;background:none!important;height:100%;width:100%;}</style> */}
+                </div>
+              </div>
+          </div>
+          <div className="mt-8">
+            <p className="text-white text-xl mb-8">India Branch</p>
+          </div>
+          <div className="mt-8">
+            <p className="text-white text-xl mb-8">Qatar Branch</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
--- src/Pages/Portfolio Details/PortfolioDetails.jsx
+++ src/Pages/Portfolio Details/PortfolioDetails.jsx
@@ -1,9 +1,164 @@
-import React from 'react'
+import React from "react";
 
 const PortfolioDetails = () => {
   return (
-    <div>PortfolioDetails</div>
-  )
-}
+    <div className="bg-black">
+      <div className="relative">
+        <img src="/images/Portfolio Details/Hero.png" alt="" />
+        <h1 className="absolute top-1/3 left-28 text-7xl text-white">
+          Medical VR Intubation
+          <br /> Simulation
+        </h1>
+      </div>
+      <div className="w-3/4 mx-auto flex justify-between mt-16">
+        <div className="w-1/4">
+          <div className="mb-10">
+            <h1 className="text-white text-xl mb-4 pb-4 border-b border-white">
+              Application Development
+            </h1>
+            <div>
+              <ul className="text-white marker:text-red-800 list-disc ml-4">
+                <li>
+                  Social Camera Effects, Filters & Lenses for Facebook,
+                  Instagram and Snapchat
+                </li>
+                <li>Web AR</li>
+                <li>Mobile AR Apps</li>
+                <li>
+                  IoT / HMD AR and AR Glasses Development for Magic Leap and
+                  HoloLens
+                </li>
+                <li>AR for Print - Murals, Posters and Packaging</li>
+              </ul>
+            </div>
+          </div>
+          <div className="mb-10">
+            <h1 className="text-white text-xl mb-4 pb-4 border-b border-white">
+              VR (Virtual Reality)
+            </h1>
+            <div>
+              <ul className="text-white marker:text-red-800 list-disc ml-4">
+                <li>
+                  VR Content Creation - Live Action 360 Video Production
+                  Services
+                </li>
+                <li>VR Content Creation - CGI and VFX</li>
+              </ul>
+            </div>
+          </div>
+          <div className="mb-10">
+            <h1 className="text-white text-xl mb-4 pb-4 border-b border-white">
+              From The Experts
+            </h1>
+            <div>
+              <ul className="text-white marker:text-red-800 list-disc ml-4">
+                <li>An Insider’s Guide to Social Augmented Reality</li>
+                <li>Facebook, Instagram AR Filters and Snapchat AR Lenses</li>
+              </ul>
+            </div>
+          </div>
+          <div className="mb-10">
+            <h1 className="text-white text-xl mb-4 pb-4 border-b border-white">
+              Questions?
+            </h1>
+            <div>
+              <ul className="text-white marker:text-red-800 list-disc ml-4">
+                <li>
+                  Feel free to reach out.
+                  <br /> <span className="font-medium"> Contact Us</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="w-4/6">
+          <div className="mb-16">
+            <h1 className="text-white text-4xl mb-8">
+              Toyota Role-Based Learning VR Program for Safety Training
+            </h1>
+            <img src="/images/Portfolio Details/detail-1.png" alt="" />
+            <p className="text-white mt-6">
+              Our team will work closely with you to understand your goals and
+              create a tailored XR prototype that meets your needs and exceeds
+              your expectations.
+            </p>
+            <p className="text-white mt-6">
+              At our XR agency, we’re passionate about innovation and
+              creativity, and we’re dedicated to helping startups bring their
+              ideas to life. If you’re ready to take your startup to the next
+              level, contact us today to learn more about our services and how
+              we can help you succeed.
+            </p>
+          </div>
+          <div className="mb-16">
+            <h1 className="text-white text-4xl mb-8">
+            Built for Desktop and HTC Focus 3
+            </h1>
+            <img src="/images/Portfolio Details/detail-2.png" alt="" />
+            <p className="text-white mt-6">
+              Our team will work closely with you to understand your goals and
+              create a tailored XR prototype that meets your needs and exceeds
+              your expectations.
+            </p>
+            <p className="text-white mt-6">
+              At our XR agency, we’re passionate about innovation and
+              creativity, and we’re dedicated to helping startups bring their
+              ideas to life. If you’re ready to take your startup to the next
+              level, contact us today to learn more about our services and how
+              we can help you succeed.
+            </p>
+          </div>
+          <div className="mb-16">
+            <h1 className="text-white text-4xl mb-8">
+            Toyota Role-Based Learning VR Program for Safety Training
+            </h1>
+            <img src="/images/Portfolio Details/detail-3.png" alt="" />
+            <p className="text-white mt-6">
+              Our team will work closely with you to understand your goals and
+              create a tailored XR prototype that meets your needs and exceeds
+              your expectations.
+            </p>
+            <p className="text-white mt-6">
+              At our XR agency, we’re passionate about innovation and
+              creativity, and we’re dedicated to helping startups bring their
+              ideas to life. If you’re ready to take your startup to the next
+              level, contact us today to learn more about our services and how
+              we can help you succeed.
+            </p>
+          </div>
+          <div className="mb-16">
+            <h1 className="text-white text-4xl mb-8">
+            Built for Desktop and HTC Focus 3
+            </h1>
+            <img src="/images/Portfolio Details/detail-4.png" alt="" />
+            <p className="text-white mt-6">
+              Our team will work closely with you to understand your goals and
+              create a tailored XR prototype that meets your needs and exceeds
+              your expectations.
+            </p>
+            <p className="text-white mt-6">
+              At our XR agency, we’re passionate about innovation and
+              creativity, and we’re dedicated to helping startups bring their
+              ideas to life. If you’re ready to take your startup to the next
+              level, contact us today to learn more about our services and how
+              we can help you succeed.
+            </p>
+          </div>
+          <div className="flex justify-between border-t border-white mt-8 py-16">
+          <div>
+            <p className="text-white text-lg">Prev</p>
+            <h2 className="text-white text-2xl">Augmented Reality</h2>
+          </div>
+          <div>
+            <p className="text-white text-right text-lg">Next</p>
+            <h2 className="text-white text-2xl">Immersive Training</h2>
+          </div>
+        </div>
+        </div>
+      </div>
+     
+    </div>
+  );
+};
 
-export default PortfolioDetails
+export default PortfolioDetails;
