385eb325e569b0dd74cca3e33affb67d38112fbc
imtiaz hussain <imtiazh993@gmail.com>
2023-03-28 11:29:33 -0700

    unused imports

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbar from "../Common/Navbar";
 import Footer from "../Common/Footer";
-import ScrollToTop from "../Common/ScrollToTop";
 
 const Layout = ({children}) => {
   return (
